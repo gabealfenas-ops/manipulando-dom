1b44ca654a64b1398dedb06275e81cf13f02811e
Gabriela Alfenas <gabealfenas@gmail.com>
2026-07-30 23:02:22 -0300

    Apresentando a responsividade

---
+++ Js/pokemon-api.js
@@ -0,0 +1,10 @@
+const formPokemon = document.getElementById("pokemon-form");
+const inputPokemon = document.getElementById("pokemon-input");
+
+formPokemon.addEventListener("submit", buscarPokemon);
+
+function buscarPokemon(event) {
+  event.preventDefault();
+
+  console.log("Ditto");
+}
--- Js/todo-list.js
+++ Js/todo-list.js
@@ -84,18 +84,25 @@ function adicionarTarefa(event) {
   btnConcluir.addEventListener("click", () => {
     texto.classList.toggle("tarefa-concluida");
 
+    const tarefaConcluida = texto.classList.contains("tarefa-concluida");
     // se na lista de classes do elemento texto contém a classe.tarefa-concluida
     // então, ele executará o bloco de código, caso contrário, não.
-    if (texto.classList.contains("tarefa-concluida")) {
+    if (tarefaConcluida) {
       iconConcluir.classList.remove("bi-check-lg");
       iconConcluir.classList.add("bi-arrow-clockwise");
 
       btnConcluir.setAttribute("aria-label", "Desmarcar tarefa");
+
+      // move o item da lista para o final da lista
+      lista.append(itemLista);
     } else {
       iconConcluir.classList.remove("bi-arrow-clockwise");
       iconConcluir.classList.add("bi-check-lg");
 
       btnConcluir.setAttribute("aria-label", "Concluir tarefa");
+
+      // move a tarefa para o inicio da lista
+      lista.prepend(itemLista);
     }
   });
 }
